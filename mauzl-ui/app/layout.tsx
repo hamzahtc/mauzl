ff7e7bad978e49600c37cfaf50fb8a7c5d02eaf6
hamzahatoch <hamzahatoch04@gmail.com>
2024-10-20 12:16:12 +0100

    integrate clarity for tracking

--- mauzl-ui/app/layout.tsx
+++ mauzl-ui/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from "./providers";
 import { Box } from "@mui/material";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Mauzl App",
@@ -17,6 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col min-h-screen">
         <Providers>
           {/*  TODO: Create a Menu for mobile version  */}
