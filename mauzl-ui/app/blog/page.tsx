1b0b146043aeaef28ce545f54c934da1aabb2bc4
hamzahatoch <hamzahatoch04@gmail.com>
2025-02-11 01:18:43 +0100

    test deployement

--- mauzl-ui/app/blog/page.tsx
+++ mauzl-ui/app/blog/page.tsx
@@ -6,7 +6,7 @@ export default function BlogPage() {
   return (
     <Stack bgcolor="#eee" px={26} minHeight="100vh">
       <Stack px={{ xs: 2, md: 10 }} py={10}>
-        THIS IS THE BLOG PAGE
+        THIS IS THE BLOG PAGE DOCKER IS OK
       </Stack>
     </Stack>
   );
